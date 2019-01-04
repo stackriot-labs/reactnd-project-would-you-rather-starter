b2027875952de3805da6733317829d3ce0ac2009
Spencer Williams <swilliamscontractor@huronconsultinggroup.com>
2019-01-03 18:37:24 -0700

    Start adding components and more node packages

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Dashboard />
+      </div>
+    );
+  }
+}
+
+export default App;

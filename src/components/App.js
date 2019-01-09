61379a5c4655b35453b095903887513d5522621d
Spencer Williams <spencer@aninternetpresence.net>
2019-01-08 19:29:14 -0700

    The questions appear in the correct categories on the Dashboard for the authed user

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
+import { setAuthedUser } from '../actions/authedUser';
 import Dashboard from './Dashboard';
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-const App = () => {
-  return (
-    <div className="App">
-    {
-      <Dashboard />
-    }
-    </div>
-  );
+const AUTHED_ID = 'tylermcginnis';
+
+class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(setAuthedUser(AUTHED_ID));
+  }
+  render() {
+    return (
+      <div className="App">
+      {
+        <Dashboard />
+      }
+      </div>
+    );
+  }
 };
 
-export default App;
+export default connect()(App);
